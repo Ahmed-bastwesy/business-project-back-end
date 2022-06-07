e2bd8725bd13cfff147f75d7d251e15c02f0e0d6
khalidghanamy <khalid.gamal.hamed@gmail.com>
2022-06-07 03:45:48 +0200

    create index function

--- app/Http/Controllers/CartController.php
+++ app/Http/Controllers/CartController.php
@@ -12,7 +12,24 @@ class CartController extends Controller
 
     public function index($userId){
 
-        
+        try{
+            if(!Cart::find($userId)){
+             return response()->json(['message' => 'invalid data'], 404);
+ 
+            }else{
+             $userProducts =  
+                 Cart::find($userId)
+                 ->with('product')
+                 ->get()->makeHidden(['created_at', 'updated_at', 'id', 'user_id', 'product_id']);
+                 $userProducts = $userProducts->map(function($item){
+                     return $item->product;
+                 });
+             $data=[ "user_id"=>$userId,'userProducts' => $userProducts];
+             return response()->json($data, 200);
+         }
+         }catch(\Exception $e){
+             return response()->json(['message' => $e->getMessage()], 500);
+         }
 
    
 }
